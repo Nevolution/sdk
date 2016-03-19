140ad7b6a07ebd7cf48d5eb6da83d37a1b7a3a62
oasisfeng <git@id.oasisfeng.com>
2016-03-20 01:16:24 +0800

    Correctly implement decorator caller identity check by signature.

--- sdk/src/main/java/com/oasisfeng/nevo/decorator/NevoDecoratorService.java
+++ sdk/src/main/java/com/oasisfeng/nevo/decorator/NevoDecoratorService.java
@@ -16,10 +16,13 @@
 
 package com.oasisfeng.nevo.decorator;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.BadParcelableException;
 import android.os.Binder;
 import android.os.Build;
@@ -36,9 +39,11 @@ import android.util.Log;
 import com.oasisfeng.nevo.StatusBarNotificationEvo;
 import com.oasisfeng.nevo.engine.INevoController;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.pm.PackageManager.GET_SIGNATURES;
+import static android.content.pm.PackageManager.SIGNATURE_MATCH;
 
 /**
  * Interface for notification decorator.
@@ -157,14 +162,6 @@ public abstract class NevoDecoratorService extends Service {
 	}
 
 	@CallSuper @Override public IBinder onBind(final Intent intent) {
-		if ((getApplicationContext().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) == 0) {
-			// TODO: Verify signature instead
-			final int caller_uid = Binder.getCallingUid();
-			if (caller_uid != Process.myUid()) {
-				final String[] pkgs = getPackageManager().getPackagesForUid(caller_uid);
-				if (pkgs == null || ! Arrays.asList(pkgs).contains(NEVO_PACKAGE_NAME)) return null;
-			}
-		}
 		for (Class<?> clazz = getClass(); clazz != NevoDecoratorService.class; clazz = clazz.getSuperclass()) {
 			try {
 				if (clazz.getDeclaredMethod("apply", StatusBarNotificationEvo.class) != null) mFlags |= FLAG_DECORATION_AWARE;
@@ -179,7 +176,7 @@ public abstract class NevoDecoratorService extends Service {
 		return mWrapper == null ? mWrapper = new INevoDecoratorWrapper() : mWrapper;
 	}
 
-	private String shorten(final String name) {
+	private static String shorten(final String name) {
 		final String suffix = "Decorator";
 		return name.endsWith(suffix) ? name.substring(0, name.length() - suffix.length()) : name;
 	}
@@ -194,12 +191,12 @@ public abstract class NevoDecoratorService extends Service {
 	/** Internal flag */ static final int FLAG_INCLUDE_NO_CLEAR = 0x8;
 	/** Internal extra */ static final String EXTRA_TAG_OVERRIDE = "nevo.tag.override";
 	/** Internal extra */ static final String EXTRA_ID_OVERRIDE = "nevo.id.override";
-	private static final String NEVO_PACKAGE_NAME = "com.oasisfeng.nevo";
 	protected final String TAG = "Nevo.Decorator[" + shorten(getClass().getSimpleName()) + "]";
 
 	private class INevoDecoratorWrapper extends INevoDecorator.Stub {
 
 		@Override public void apply(final StatusBarNotificationEvo evolving, final @Nullable Bundle options) {
+			if (Binder.getCallingUid() != mCallerUid) throw new SecurityException();
 			try {
 				Log.v(TAG, "Applying to " + evolving.getKey());
 				final String original_tag = evolving.getTag(); final int original_id = evolving.getId();
@@ -215,19 +212,8 @@ public abstract class NevoDecoratorService extends Service {
 			}
 		}
 
-		@Override public int onConnected(final INevoController controller, final @Nullable Bundle options) {
-			mController = controller;
-			try {
-				Log.v(TAG, "onConnected");
-				NevoDecoratorService.this.onConnected();
-			} catch (final Throwable t) {
-				Log.e(TAG, "Error running onConnected()", t);
-				throw asParcelableException(t);
-			}
-			return mFlags;
-		}
-
 		@Override public void onNotificationRemoved(final String key, final @Nullable Bundle options) {
+			if (Binder.getCallingUid() != mCallerUid) throw new SecurityException();
 			try {
 				NevoDecoratorService.this.onNotificationRemoved(key);
 			} catch (final Throwable t) {
@@ -237,12 +223,39 @@ public abstract class NevoDecoratorService extends Service {
 		}
 
 		@Override public void onNotificationRemovedLight(final StatusBarNotificationEvo notification, final @Nullable Bundle options) {
+			if (Binder.getCallingUid() != mCallerUid) throw new SecurityException();
 			try {
 				NevoDecoratorService.this.onNotificationRemoved(notification);
 			} catch (final Throwable t) {
 				Log.e(TAG, "Error running onNotificationRemoved()", t);
 				throw asParcelableException(t);
 			}
+		}
+
+		@Override public int onConnected(final INevoController controller, final @Nullable Bundle options) {
+			final PackageManager pm = getPackageManager();
+			final int caller_uid = Binder.getCallingUid(), my_uid = Process.myUid();
+			if (caller_uid != my_uid && pm.checkSignatures(caller_uid, my_uid) != SIGNATURE_MATCH) {
+				final String[] caller_pkgs = pm.getPackagesForUid(caller_uid);
+				if (caller_pkgs == null || caller_pkgs.length == 0) throw new SecurityException();
+				try { @SuppressLint("PackageManagerGetSignatures")
+				final PackageInfo caller_info = pm.getPackageInfo(caller_pkgs[0], GET_SIGNATURES);
+					if (caller_info == null) throw new SecurityException();
+					for (final Signature signature : caller_info.signatures)
+						if (signature.hashCode() != SIGNATURE_HASH) throw new SecurityException("Caller signature mismatch");
+				} catch (final PackageManager.NameNotFoundException e) { throw new SecurityException(); }	// Should not happen
+			}
+			mCallerUid = caller_uid;
+
+			mController = controller;
+			try {
+				Log.v(TAG, "onConnected");
+				NevoDecoratorService.this.onConnected();
+			} catch (final Throwable t) {
+				Log.e(TAG, "Error running onConnected()", t);
+				throw asParcelableException(t);
+			}
+			return mFlags;
 		}
 
 		private boolean equals(final Object a, final Object b) {
@@ -260,5 +273,8 @@ public abstract class NevoDecoratorService extends Service {
 				return (RuntimeException) e;
 			return new IllegalStateException(e);
 		}
+
+		private int mCallerUid = -1;
+		private static final int SIGNATURE_HASH = -541181501;
 	}
 }
